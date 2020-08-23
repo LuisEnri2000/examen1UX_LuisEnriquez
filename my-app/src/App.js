2bc64b3f7c9d8238de16dbe9890a03bab281019e
LuisEnri2000 <qubicgamer@gmail.com>
2020-08-22 23:59:44 -0600

    Bases de la tabla
    Aqui intento crear el formato de la tabla con react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import DataTable from 'react-data-table-component'
+
+const tablaRecia =[
+  {apunte:"React es una biblioteca de javascript para construir interfaces de usuario.", fecha:"21/ago/2020 09:31:50", etiquetas:"react, javascript, front-end"},
+
+];
+
+const columnasRecias = [
+  {
+    name: 'Recio', 
+    selector: 'apunte', 
+    sortable: true 
+  },
+  {
+    name: 'Fecha', selector: 'fecha', sortable: true 
+  },
+  {
+    name: 'Etiquetas', selector: 'etiquetas', sortable: true
+  },
+]
 
 function App() {
   return (
     <div className="App">
 
-      <h1>Hello, world!</h1>
+      <DataTable 
+      columns = {columnasRecias}
+      data = {tablaRecia}
+      title = "Recio"
+      />
       
 
-  </div>
+    </div>
   );
 }
 
